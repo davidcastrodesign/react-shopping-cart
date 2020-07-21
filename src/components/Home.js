5914ab7fca162f9463f1c8c85b1a8be102c97c08
David Castro Castaneda <dcastrocast@gmail.com>
2020-07-21 03:13:09 -0700

    add images css home component

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <header>
+      <div className='overlay'></div>
+      <nav>
+        <h2>Shop</h2>
+        <ul>
+          <li>
+            <a href='#'>Home</a>
+          </li>
+          <li>
+            <a href='#'>About</a>
+          </li>
+          <li className='cart'>
+            <a href='#'>
+              <ion-icon name='basket-outline'></ion-icon>Cart<span></span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Home;
